0d221f60004beb8513c2c1e3fe4100f3f9d857c7
bmizerany <blake.mizerany@gmail.com>
2008-07-17 12:50:46 -0700

    isolated tests

--- test/filter_test.rb
+++ test/filter_test.rb
@@ -1,162 +1,52 @@
 require File.dirname(__FILE__) + "/helper"
 
 context "Filters when falling" do
+
+  specify "returns 200 by default" do
+
+    filter = Sinatra::Filter.new do
+      'hi'
+    end
+
+    status, _, _ = filter.call(context_for("/"))
+
+    assert_equal(200, status)
+    
+  end
+
+  specify "does not touch headers by default" do
+
+    filter = Sinatra::Filter.new do
+      'hi'
+    end
+
+    _, headers, _ = filter.call(context_for("/"))
+
+    ## This header is the default header entered by Rack
+    assert_equal({'Content-Type' => 'text/html'}, headers)
+    
+  end
   
-  specify "should fall through to the next app" do
+  specify "sets the body to the return value of the block" do
     
-    app = Sinatra::Application.new do
-      
-      filter do
-        @foo = 'foo'
-        fall
-      end
-      
-      group do
-        
-        filter do
-          @foo
-        end
-        
-      end
-
+    filter = Sinatra::Filter.new do
+      'hi'
     end
     
-    assert_equal(2, app.events.size)
-    assert_equal(1, app.events.last.events.size)
+    _, _, response = filter.call(context_for("/"))
     
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')
-    
-    assert_equal(200, response.status)
-    assert_equal('foo', response.body)
+    assert_equal('hi', response.body)
     
   end
-
-
-  specify "should 404 if the app bottoms-out" do
-    
-    app = Sinatra::Application.new do
-      
-      filter do
-        @foo = 'foo'
-        fall
-      end
-      
-      group do
-        
-        filter do
-          @foo
-          fall
-        end
-        
-      end
-
-    end
-
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')
-    
-    assert_equal(99, response.status)
-    
-  end
-
-  specify "should be successful falls from inner app and hits good filter" do
-    
-    app = Sinatra::Application.new do
-            
-      group do
-        
-        filter do
-          @foo = 'inner foo'
-          fall
-        end
-        
-      end
-      
-      filter do
-        @foo
-      end
-
-    end
-
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')
-    
-    assert_equal(200, response.status)
-    assert_equal('inner foo', response.body)
-    
-  end
-
-  specify "should be successful when falling through to next app" do
-    
-    app = Sinatra::Application.new do
-            
-      group do
-        
-        filter do
-          @foo = 'inner foo'
-          fall
-        end
-        
-      end
-      
-      filter do
-        @foo
-      end
-
-    end
-
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')
-    
-    assert_equal(200, response.status)
-    assert_equal('inner foo', response.body)
-    
-  end
-
-  specify "should be successful when falling through to next app" do
-    
-    app = Sinatra::Application.new do
-            
-      group do
-        
-        filter do
-          @foo = 'inner foo'
-          fall_group
-        end
-        
-        filter do
-          @foo = 'not it'
-        end
-        
-      end
-      
-      filter do
-        @foo
-      end
-
-    end
-
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')
-    
-    assert_equal(200, response.status)
-    assert_equal('inner foo', response.body)
-    
-  end
+  
   
   specify "gets it's daily meta" do
     
-    app = Sinatra::Application.new do
-      
-      filter :some_meta => 'foo' do
-        options[:some_meta]
-      end
-
+    filter = Sinatra::Filter.new :foo => 'foo' do
+      options[:foo]
     end
     
-    request   = Rack::MockRequest.new(app)
-    response  = request.get('/')    
+    _, _, response = filter.call(context_for("/"))
     
     assert_equal('foo', response.body)
     
