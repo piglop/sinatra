b96d745db4225e6d261c79b5658576f10537494e
bmizerany <blake.mizerany@gmail.com>
2008-07-17 18:21:34 -0700

    Erb is in

--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -107,9 +107,38 @@ module Sinatra
 
 
 
+  module Erb
+    
+    def erb(content, options={})
+      require 'erb'
+      render(:erb, content, options)
+    end
+    
+    private 
+    
+      def render_erb(content, options = {})
+        locals_opt = options.delete(:locals) || {}
+
+        locals_code = ""
+        locals_hash = {} 
+        locals_opt.each do |key, value|
+          locals_code << "#{key} = locals_hash[:#{key}]\n"
+          locals_hash[:"#{key}"] = value
+        end
+ 
+        body = ::ERB.new(content).src
+        eval("#{locals_code}#{body}", binding)
+      end
+
+  end
+
+
+
+
   class EventContext
     include RenderingHelpers
     include Haml
+    include Erb
     
     attr_reader   :request, :response
     attr_accessor :options
